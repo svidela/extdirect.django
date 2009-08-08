0314c61da1edc9e22414ff6c9bc954afc25bdc84
Santiago <santiago.videla@gmail.com>
2009-08-08 11:38:49 -0300

    Add the extdirect django serializer to be use it from ExtDirectStore

--- extdirect/django/__init__.py
+++ extdirect/django/__init__.py
@@ -2,3 +2,4 @@
 from decorators import remoting, polling
 from providers import ExtRemotingProvider, ExtPollingProvider
 from store import ExtDirectStore
+
--- extdirect/django/store.py
+++ extdirect/django/store.py
@@ -1,5 +1,94 @@
-from django.core.serializers import serialize
+from django.core.serializers import serialize, python
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+
+from StringIO import StringIO
+from django.utils.encoding import smart_str, smart_unicode
+from django.utils import datetime_safe
+
+class Serializer(python.Serializer):
+    """    
+    """
+    def start_serialization(self, total):
+        self._current = None
+        self.objects = {
+            self.meta['root']: [],
+            self.meta['total']: total
+        }
+
+    def end_serialization(self):
+        pass
+
+    def start_object(self, obj):
+        self._current = {}
+        
+    def end_object(self, obj):
+        rec = self._current
+        rec['id'] = smart_unicode(obj._get_pk_val(), strings_only=True)
+        
+        for extra in self.extras:
+                rec[extra[0]] = extra[1](obj)
+                
+        self.objects[self.meta['root']].append(rec)
+        self._current = None        
+        
+    def handle_field(self, obj, field):
+        self._current[field.name] = smart_unicode(getattr(obj, field.name), strings_only=True)
+
+    def handle_fk_field(self, obj, field):
+        related = getattr(obj, field.name)
+        if related is not None:
+            if field.rel.field_name == related._meta.pk.name:
+                # Related to remote object via primary key
+                related = related._get_pk_val()
+            else:
+                # Related to remote object via other field
+                related = getattr(related, field.rel.field_name)
+        self._current[field.name] = self._current[field.name+'_id'] = smart_unicode(related, strings_only=True)         
+
+    def handle_m2m_field(self, obj, field):
+        if field.creates_table:
+            self._current[field.name] = self._current[field.name+'_ids'] = [smart_unicode(related._get_pk_val(), strings_only=True)
+                               for related in getattr(obj, field.name).iterator()]        
+    
+    def serialize(self, queryset, **options):
+        """
+        Serialize a queryset.
+        """
+        self.options = options
+
+        self.stream = options.get("stream", StringIO())
+        self.selected_fields = options.get("fields")
+        
+        self.local_fields = options.get("local")
+        
+        self.meta = options.get('meta', dict(root='records', total='total'))
+        self.extras = options.get('extras', [])
+        
+        total = options.get("total", queryset.count())        
+        self.start_serialization(total)
+                        
+        for obj in queryset:
+            if self.local_fields:
+                fields = obj._meta.local_fields
+            else:
+                fields = obj._meta.fields
+                
+            self.start_object(obj)
+            for field in fields:
+                if field.serialize:
+                    if field.rel is None:
+                        if self.selected_fields is None or field.attname in self.selected_fields:
+                            self.handle_field(obj, field)
+                    else:
+                        if self.selected_fields is None or field.attname[:-3] in self.selected_fields:
+                            self.handle_fk_field(obj, field)
+            for field in obj._meta.many_to_many:
+                if field.serialize:
+                    if self.selected_fields is None or field.attname in self.selected_fields:
+                        self.handle_m2m_field(obj, field)
+            self.end_object(obj)
+        self.end_serialization()
+        return self.getvalue()    
 
 class ExtDirectStore(object):
     """
@@ -48,7 +137,7 @@ class ExtDirectStore(object):
         self.sort = sort
         self.dir = dir
         
-    def query(self, **kw):                
+    def query(self, qs=None, **kw):                
         paginate = False
         total = None
         order = False
@@ -64,9 +153,17 @@ class ExtDirectStore(object):
             order = True
             
             if dir == 'DESC':
-                sort = '-' + sort            
+                sort = '-' + sort
                 
-        queryset = self.model.objects.filter(**kw)
+        if not qs is None:
+            # Don't use queryset = qs or self.model.objects
+            # because qs could be empty list (evaluate to False)
+            # but it's actually an empty queryset that must have precedence
+            queryset = qs
+        else:
+            queryset = self.model.objects
+            
+        queryset = queryset.filter(**kw)
         
         if order:
             queryset = queryset.order_by(sort)
@@ -88,21 +185,9 @@ class ExtDirectStore(object):
         return self.serialize(objects, total)
         
     def serialize(self, queryset, total=None):        
-        res = serialize('python', queryset)
-        
-        # set initial state for output
-        out = {
-            self.root: [],
-            self.total: total or queryset.count()
+        meta = {
+            'root': self.root,
+            'total' : self.total
         }
-        
-        # for each field, append id into field and append to output records 
-        for record in res:                        
-            ext_rec = record['fields']
-            ext_rec['id'] = record['pk']
-            for extra in self.extras:
-                ext_rec[extra[0]] = extra[1](record)
-                
-            out[self.root].append(ext_rec)
-        
-        return out
+        res = serialize('extdirect', queryset, meta=meta, extras=self.extras)
+        return res
